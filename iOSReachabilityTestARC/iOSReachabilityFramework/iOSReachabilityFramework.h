fb7eb81127f25f43b2d652de60715e9b3507e843
Nate Minshew <nate.minshew@asolutions.com>
2012-12-12 10:51:42 -0600

    Added framework target.

---
+++ iOSReachabilityTestARC/iOSReachabilityFramework/iOSReachabilityFramework.h
@@ -0,0 +1,13 @@
+//
+//  iOSReachabilityFramework.h
+//  iOSReachabilityFramework
+//
+//  Created by Nate Minshew on 12/12/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iOSReachabilityFramework : NSObject
+
+@end
---
+++ iOSReachabilityTestARC/iOSReachabilityFramework/iOSReachabilityFramework.m
@@ -0,0 +1,13 @@
+//
+//  iOSReachabilityFramework.m
+//  iOSReachabilityFramework
+//
+//  Created by Nate Minshew on 12/12/12.
+//
+//
+
+#import "iOSReachabilityFramework.h"
+
+@implementation iOSReachabilityFramework
+
+@end
